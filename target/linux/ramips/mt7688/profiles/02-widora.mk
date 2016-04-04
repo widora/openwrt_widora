305275ff4948a1d88bb3cc9384579b3704ac1d8a
mango <771992497@qq.com>
2016-04-04 22:12:44 +0800

    add exfat,remove avrdude

--- target/linux/ramips/mt7688/profiles/02-widora.mk
+++ target/linux/ramips/mt7688/profiles/02-widora.mk
@@ -14,8 +14,8 @@ define Profile/Widora
         	mjpg-streamer \
 		uhttpd rpcd rpcd-mod-iwinfo \
 		luci luci-app-mjpg-streamer luci-app-samba luci-lib-json \
-		rpcd-mod-rpcsys cgi-io avrdude spi-tools \
-		kmod-fs-vfat kmod-i2c-core kmod-i2c-ralink \
+		rpcd-mod-rpcsys cgi-io spi-tools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
 		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
 		kmod-sdhci-mt7620 kmod-usb-storage \
 		kmod-video-core kmod-video-uvc \
