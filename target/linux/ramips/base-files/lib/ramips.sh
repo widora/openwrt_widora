6115e4578a2b1718a838ab626b2d3b7de2411ad0
mango <771992497@qq.com>
2016-08-30 23:08:21 +0800

    add 32MB winbond flash support,the menuconfig is Widora32M

--- target/linux/ramips/base-files/lib/ramips.sh
+++ target/linux/ramips/base-files/lib/ramips.sh
@@ -430,6 +430,9 @@ ramips_board_detect() {
 	*"Widora-NEO")
 		name="widora"
 		;;
+	*"Widora32M")
+		name="widora32m"
+		;;
 	*"MediaTek LinkIt Smart 7688")
 		linkit="$(dd bs=1 skip=1024 count=12 if=/dev/mtd2 2> /dev/null)"
 		if [ "${linkit}" = "LINKITS7688D" ]; then
