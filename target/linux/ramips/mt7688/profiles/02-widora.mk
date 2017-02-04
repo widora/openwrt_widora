a12df0e275cb72c071b13a71a6f5e05147233d15
mango <widora@qq.com>
2017-02-04 17:13:42 +0800

    chagne mtk-wifi/Makefile compile error,use mtk-wifi default

--- target/linux/ramips/mt7688/profiles/02-widora.mk
+++ target/linux/ramips/mt7688/profiles/02-widora.mk
@@ -20,7 +20,7 @@ define Profile/Widora
 		kmod-sdhci-mt7620 kmod-usb-storage \
 		kmod-video-core kmod-video-uvc \
 		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
-		mt7628WIFI airkiss webui\
+		ap-iwinfo mtk-wifi airkiss webui\
         	maccalc shairport_mmap reg
 endef
 
